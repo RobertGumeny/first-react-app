8ac30c336ee279f9119468021bd01604a6d95ccb
Robert Gumeny <robert.gumeny528@gmail.com>
2020-06-16 18:57:10 -0600

    Need to change team assignment on button click

--- src/App.js
+++ src/App.js
@@ -6,18 +6,63 @@ class App extends Component {
 
   state = {
     people: [
-      { name: 'Steven', age: 35 },
-      { name: 'Robert', age: 32 },
-      { name: 'Alison', age: 24 }
+      { name: 'Steven', age: 35, team: 'Unassigned' },
+      { name: 'Robert', age: 32, team: 'Unassigned' },
+      { name: 'Alison', age: 24, team: 'Unassigned' },
+      { name: 'Harvey', age: 42, team: 'Unassigned' },
+      { name: 'Stephanie', age: 28, team: 'Unassigned' },
+      { name: 'Susan', age: 37, team: 'Unassigned' }
     ]
   }
+
+  assignTeamHandler = () => {
+    console.log("go team!")
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}>My Hobbies: Playing Poker</Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Fishing</Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}>My Hobbies: Dancing</Person>
+        <div className="personRow">
+          <Person
+            name={this.state.people[0].name}
+            age={this.state.people[0].age}
+            team={this.state.people[0].team}
+            click={this.assignTeamHandler} />
+          <Person
+            name={this.state.people[1].name}
+            age={this.state.people[1].age}
+            team={this.state.people[1].team}
+            click={this.assignTeamHandler} />
+          <Person
+            name={this.state.people[2].name}
+            age={this.state.people[2].age}
+            team={this.state.people[2].team}
+            click={this.assignTeamHandler} />
+          <Person
+            name={this.state.people[3].name}
+            age={this.state.people[3].age}
+            team={this.state.people[3].team}
+            click={this.assignTeamHandler} />
+          <Person
+            name={this.state.people[4].name}
+            age={this.state.people[4].age}
+            team={this.state.people[4].team}
+            click={this.assignTeamHandler} />
+          <Person
+            name={this.state.people[5].name}
+            age={this.state.people[5].age}
+            team={this.state.people[5].team}
+            click={this.assignTeamHandler} />
+        </div>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import './Person.css'
 
 export default class Person extends Component {
   render() {
     return (
-      <div>
+      <div className="Person">
         <p>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        <p>{this.props.children}</p>
+        <p>Team: {this.props.team}</p>
+        <div className="teamButtons">
+          <button>Red Team</button>
+          <button>Blue Team</button>
+        </div>
       </div>
     )
   }
