3a632ef2cd3da7b6a185be22554889fe94a33079
Robert Gumeny <robert.gumeny528@gmail.com>
2020-06-16 20:05:27 -0600

    People are now assigned by ID

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ class App extends Component {
 
   state = {
     people: [
-      { name: 'Steven', age: 35, team: 'Unassigned' },
-      { name: 'Robert', age: 32, team: 'Unassigned' },
-      { name: 'Alison', age: 24, team: 'Unassigned' },
-      { name: 'Harvey', age: 42, team: 'Unassigned' },
-      { name: 'Stephanie', age: 28, team: 'Unassigned' },
-      { name: 'Susan', age: 37, team: 'Unassigned' }
+      { id: 0, name: 'Steven', age: 35, team: 'Unassigned' },
+      { id: 1, name: 'Robert', age: 32, team: 'Unassigned' },
+      { id: 2, name: 'Alison', age: 24, team: 'Unassigned' },
+      { id: 3, name: 'Harvey', age: 42, team: 'Unassigned' },
+      { id: 4, name: 'Stephanie', age: 28, team: 'Unassigned' },
+      { id: 5, name: 'Susan', age: 37, team: 'Unassigned' }
     ]
   }
 
-  assignBlueTeamHandler = (personIndex) => {
+  assignBlueTeamHandler = (id) => {
     let people = this.state.people
-    people[personIndex].team = 'Blue'
+    people.forEach(p => {
+      if (p.id === id) {
+        p.team = 'Blue'
+      }
+    })
     this.setState({ people: people })
   }
 
-  assignRedTeamHandler = (personIndex) => {
+  assignRedTeamHandler = (id) => {
     let people = this.state.people
-    people[personIndex].team = 'Red'
+    people.forEach(p => {
+      if (p.id === id) {
+        p.team = 'Red'
+      }
+    })
     this.setState({ people: people })
   }
+
+
 
   render() {
     const style = {
@@ -39,15 +49,37 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
+        <p>Use the buttons to assign people to the <span style={{ color: 'red' }}>Red</span> or <span style={{ color: 'blue' }}>Blue</span> team!</p>
         <div className="personRow">
-          {this.state.people.map((p, index) => {
+          {this.state.people.filter(p => p.team === 'Unassigned').map(p => {
             return <Person
-              blueTeam={() => this.assignBlueTeamHandler(index)}
-              redTeam={() => this.assignRedTeamHandler(index)}
+              blueTeam={() => this.assignBlueTeamHandler(p.id)}
+              redTeam={() => this.assignRedTeamHandler(p.id)}
               name={p.name}
               age={p.age}
               team={p.team} />
           })}
+        </div>
+        <div className="teamRow">
+          <div className="redTeam">
+            <h2>Red Team:</h2>
+            {this.state.people.filter(p => p.team === 'Red').map(p => {
+              return <Person
+                name={p.name}
+                age={p.age}
+                team={p.team} />
+            })}
+          </div>
+          <div className="blueTeam">
+            <h2>Blue Team:</h2>
+            {this.state.people.filter(p => p.team === 'Blue').map(p => {
+              return <Person
+                name={p.name}
+                age={p.age}
+                team={p.team} />
+            })}
+          </div>
+
         </div>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,15 +2,31 @@ import React, { Component } from 'react'
 import './Person.css'
 
 export default class Person extends Component {
+
   render() {
-    return (
-      <div className="Person">
-        <p>I'm {this.props.name} and I am {this.props.age} years old!</p>
-        <p>Team: {this.props.team}</p>
+    let team = this.props.team
+    let teamDisplay = null;
+    let buttons = null;
+    if (team === 'Unassigned') {
+      buttons = (
         <div className="teamButtons">
           <button onClick={this.props.redTeam}>Red Team</button>
           <button onClick={this.props.blueTeam}>Blue Team</button>
         </div>
+      )
+      teamDisplay = (
+        <p>Team: {this.props.team}</p>
+      )
+    } else {
+      buttons = null;
+      teamDisplay = null;
+    }
+    return (
+      <div className="Person">
+        <p>Name: {this.props.name}</p>
+        <p>Age: {this.props.age}</p>
+        {teamDisplay}
+        {buttons}
       </div>
     )
   }
