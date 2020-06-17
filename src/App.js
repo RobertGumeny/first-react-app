662acd60c72a3ece9ec8ac346cfe46bc654bf697
Robert Gumeny <robert.gumeny528@gmail.com>
2020-06-16 20:22:08 -0600

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -38,13 +38,6 @@ class App extends Component {
 
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     return (
       <div className="App">
